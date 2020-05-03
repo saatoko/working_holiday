d642ce20a0bbe41e8399dc0fd0e74de54993b385
saatoko <rm-as.l2j@ezweb.ne.jp>
2020-05-03 10:06:40 +0900

    Create data with seed_fu, Change situations table and relation

--- db/migrate/20200501063703_create_situations.rb
+++ db/migrate/20200501063703_create_situations.rb
@@ -1,8 +1,7 @@
 class CreateSituations < ActiveRecord::Migration[6.0]
   def change
     create_table :situations do |t|
-      t.references :country,   null: false, foreign_key: true
-      t.string     :situation, null: false
+      t.string :situation, null: false
       t.timestamps
     end
   end
