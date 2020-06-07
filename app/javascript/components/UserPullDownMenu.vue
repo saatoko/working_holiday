7bbd4b1ff2bbe29dba5fb22c42ce07d8fe140128
saatoko <rm-as.l2j@ezweb.ne.jp>
2020-06-07 19:23:13 +0900

    ユーザーアイコンからのプルダウンメニュー

--- app/javascript/components/UserPullDownMenu.vue
+++ app/javascript/components/UserPullDownMenu.vue
@@ -1,12 +1,10 @@
 <script>
-import anime from "animejs"
-// import anime from 'animejs/lib/anime.es.js'
+import anime from 'animejs/lib/anime.es.js'
 
 export default {
   data: function () {
     return {
-      // el: '#user-pull',
-      isShown: 'false',
+      isShown: false,
       name: 'ユーザーメニュー',
       items: [
         'プロフィール編集',
@@ -20,44 +18,46 @@ export default {
   },
 
   template:`
-  <div>
-    <ul @mouseleave="isShown = false">
-      <li @mouseover="isShown = true"><v-fa icon="user"></v-fa></li>
-        <transition
-          @before-enter="beforeEnter"
-          @enter="enter"
-          @leave="leave"
-          :css="false"
-        >
-          <ul v-if="isShown">
-            <li class="menu">{{name}}</li>
-            <li v-for="item in items" :key="item">
-              <a href="#" class="menu-item">{{item}}</a>
-            </li>
-          </ul>
-        </transition>
-      </li>
-    </ul>
-  </div>
+    <div @mouseleave="isShown = false">
+      <ul @mouseover="isShown = true">
+        <v-fa icon="user" class="user-icon">
+        </v-fa>
+      </ul>
+
+      <transition
+        v-on:before-enter="beforeEnter"
+        v-on:enter="enter"
+        v-on:leave="leave"
+        :css="false"
+      >
+        <ul v-if="isShown" class="show-usermenu">
+          <li class="menu">{{name}}
+          </li>
+          <li v-for="item in items" :key="item">
+            <a href="#" class="menu-item">{{item}}</a>
+          </li>
+        </ul>
+      </transition>
+    </div>
   `,
 
-  method: {
+  methods: {
     beforeEnter: function (el) {
       el.style.height = '0px'
-      el.style.opacity = 0
+      el.style.opacity = '0'
     },
     enter: function (el,done) {
       anime({
-        target: el,
+        targets: el,
         opacity: 1,
         height: el.scrollHeight + 'px',
         duration: 3000,
-        compute: done
+        complete: done
       })
     },
-    leave: function (el,done) {
+    leave: function (el, done) {
       anime({
-        target: el,
+        targets: el,
         opacity: 0,
         height: '0px',
         duration: 3000,
@@ -69,45 +69,40 @@ export default {
 </script>
 
 <style scoped lang="scss" scoped>
-div, ul, li {
+div, ul, li, p, .user-icon{
   margin: 0;
   padding: 0;
 }
-ul {
+.user-icon {
+  font-size: 2rem;
+}
+
+.show-usermenu {
   list-style-type: none;
   margin: 0;
   padding: 0;
   font-size: 14px;
-}
-div {
-  width: 90px;
-}
-
-.menu {
-  // width: 90px;
+  .menu {
   width: 150px;
   text-decoration: none;
-  background-color: #9999FF;
   color: #000;
   border: solid 1px #6666cc;
   display: block;
   height: 30px;
   line-height: 30px;
   text-align: center;  
-  // position: relative;
-
-}
-.menu-item {
-  // position: absolute;
-  width: 90px;
-  text-decoration: none;
-  background-color: #CCCCFF;
-  color: #000;
-  border: solid 1px #6666CC;
-  border-top: none;
-  display: block;
-  height: 30px;
-  line-height: 30px;
-  text-align: center;
+  }
+  .menu-item {
+    width: 150px;
+    text-decoration: none;
+    background-color: #CCCCFF;
+    color: #000;
+    border: solid 1px #6666CC;
+    border-top: none;
+    display: block;
+    height: 30px;
+    line-height: 30px;
+    text-align: center;
+  }
 }
 </style>
